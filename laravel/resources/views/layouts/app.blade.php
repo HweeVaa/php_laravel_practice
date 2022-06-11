610b2530ae9f1ab925b2b1a276d003973ce4ac1e
HweeVaa <gnlqkgnlgus@gmail.com>
2022-06-12 02:10:17 +0900

    3rd commit

--- laravel/resources/views/layouts/app.blade.php
+++ laravel/resources/views/layouts/app.blade.php
@@ -5,11 +5,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script src="{{ mix('js/app.js') }}" defer></script>
     <title>Laravel App - @yield('title')</title>
 </head>
 
 <body>
-    <div>@yield('content')</div>
+    <div>
+        @if (session('status'))
+            <div style="background: red;color:white">
+                {{ session('status') }}
+            </div>
+        @endif
+        @yield('content')
+    </div>
 </body>
 
 </html>
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostsController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use PHPUnit\Framework\Constraint\IsFalse;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,23 +18,99 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home.index', []);
-})->name('home.index');
+// Route::get('/', function () {
+//     return view('home.index', []);
+// })->name('home.index');
 
-Route::get('/contact',function (){
-    return view('home.contact');
-})->name('home.index');
+// Route::get('/contact',function (){
+//     return view('home.contact');
+// })->name('home.index');
 
-/*Route parameters*/
-Route::get('/posts/{id}', function ($id){
-    return 'Blog post '.$id;
-})
-// ->where([
-//     'id' => '[0-9]+'
-// ])
-->name('posts.show');
+Route::get('/',[HomeController::class,'home'])
+->name('home.index');
+Route::get('/contact',[HomeController::class,'contact'])
+->name('home.contact');
+
+//single action controller
+Route::get('/single', AboutController::class);
+
+$posts = [
+    1 => [
+        'title' => 'Intro to Laravel',
+        'content' => 'This is a short intro to Laravel',
+        'is_new' => true,
+        'has_comments'=> true
+    ],
+    2 => [
+        'title' => 'Intro to PHP',
+        'content' => 'This is a short intro to PHP',
+        'is_new' => false
+    ],
+    3 => [
+        'title' => 'Intro to Golang',
+        'content' => 'This is a short intro to Golang',
+        'is_new' => false
+    ]
+    
+];
+
+Route::resource('posts',PostsController::class);
+    // ->only(['index','show','create','store', 'edit', 'update']);
+
+// Route::get('/posts',function() use ($posts){
+//     // dd(request()->all());
+//     dd((int)request()->query('page',1));
+//     return view('posts.index',['posts'=>$posts]);
+// });
+
+// /*Route parameters*/
+// Route::get('/posts/{id}', function ($id)use($posts){
+
+//     abort_if(!isset($posts[$id]),404);
+
+
+//     return view('posts.show',['post'=>$posts[$id]]);
+// })
+// // ->where([
+// //     'id' => '[0-9]+'
+// // ])
+// ->name('posts.show');
 
 Route::get('/recent-posts/{days_ago?}',function ($daysAgo=20){
     return 'Post from '. $daysAgo . ' days ago';
 })->name('posts.recent.index');
+
+
+
+// grouped fun routes
+Route::prefix('/fun')->name('fun.')->group(function() use($posts) {
+    Route::get('responses',function() use($posts){
+        return response($posts,201)
+        ->header('Content-Type','application/json')
+        ->cookie('MY_COOKIE', 'Hwee', 3600);
+    })->name('responses');
+    
+    Route::get('redirect', function(){
+        return redirect('/contact');
+    })->name('redirect');
+    
+    Route::get('back', function(){
+        return back();
+    })->name('back');
+    
+    Route::get('named-route', function(){
+        return redirect()->route('posts.show',['id'=>1]);
+    })->name('named-route');
+    
+    Route::get('away', function(){
+        return redirect() -> away('https://google.com');
+    })->name('away');
+    
+    Route::get('json', function() use($posts){
+        return response() ->json($posts);
+    })->name('json');
+    
+    Route::get('download', function() use($posts){
+        return response() ->download(public_path('/daniel.jpg'), 'face.jpg');
+    })->name('download');
+});
